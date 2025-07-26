f1d83efb5817c2e9b82580365a436a7d2d2c233a
Bubbu Dert <139119578+BubbuDert249@users.noreply.github.com>
2025-07-26 16:14:30 +0200

    Create CSharpConsoleClient.cs

---
+++ CSharpConsole_sbox/code/CSharpConsoleClient.cs
@@ -0,0 +1,50 @@
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace CSharpConsoleAddon
+{
+    public partial class CSharpConsoleClient : Client
+    {
+        private CSharpConsole consolePanel;
+        private bool isVisible = false;
+
+        public CSharpConsoleClient()
+        {
+            StyleSheet.Load("/csharp_console/ui/scss/CSharpConsole.scss");
+            // Bind key H to toggle the console
+            Input.RegisterButton( "ToggleCSharpConsole", InputButton.Down );
+
+            // Optionally add console command to toggle
+            ConsoleSystem.Register("csharp_console", () =>
+            {
+                ToggleConsole();
+            });
+        }
+
+        [Event.Tick.Client]
+        public void OnTick()
+        {
+            if (Input.Pressed(InputButton.Down) && Input.Down("ToggleCSharpConsole"))
+            {
+                ToggleConsole();
+            }
+        }
+
+        private void ToggleConsole()
+        {
+            if (isVisible)
+            {
+                consolePanel?.Delete();
+                consolePanel = null;
+                isVisible = false;
+            }
+            else
+            {
+                consolePanel = new CSharpConsole();
+                RootPanel.AddChild(consolePanel);
+                isVisible = true;
+            }
+        }
+    }
+}
